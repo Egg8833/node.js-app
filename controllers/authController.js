af1eec626c40b46d5d0181f159c80451b97e9b37
黃禎平 <egg8833@gmail.com>
2025-02-20 09:44:55 +0800

    [feat] 安裝 防護套件 xss helmet

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,17 @@ exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
   const token = singToken(newUser._id);
+  const cookieOptions = {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRE_IN * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+      secure: true,
+    };
+
+    res.cookie('jwt', token, cookieOptions);
+
+  newUser.password = undefined;
 
   res.status(201).json({
     status: 'success',
@@ -90,6 +101,7 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
